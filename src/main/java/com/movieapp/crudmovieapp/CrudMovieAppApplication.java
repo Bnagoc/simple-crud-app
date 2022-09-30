ee0e4ab3f271561e4cdb11bf6a0dbf555f656b44
Bnag <uladzislau.samuseu@gmail.com>
2022-09-30 15:14:59 +0300

    spring crud set up

---
+++ src/main/java/com/movieapp/crudmovieapp/CrudMovieAppApplication.java
@@ -0,0 +1,13 @@
+package com.movieapp.crudmovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMovieAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieapp/crudmovieapp/CrudMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieapp.crudmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
